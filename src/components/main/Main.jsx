6dba55da4b01028564a2001a041e9c09d1296f30
VCAngel <vargascasavantesangel.329@gmail.com>
2022-03-23 04:44:53 -0700

    General idea for threeJS react component; pending

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,16 +2,50 @@ import React, { Component } from 'react';
 import * as Three from 'three';
 import Blackhole from './bodies/Blackhole';
 
+//TODO threeJS models and scenes
 export default class Main extends Component {
     constructor(props) {
         super(props);
+        this.threeContainer = React.createRef(); // <== main container
+        this.initThree();
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    initThree = () => {
+        this.scene = new Three.Scene();
+        this.camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1);
+        this.renderer = new Three.WebGLRenderer();
+    }
+
+    animate() {
+        requestAnimationFrame(animate);
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    componentDidMount = () => {
+        this.bodiesRender(this?.renderer, this?.threeContainer)
+    }
+
+    bodiesRender = (renderer, ref) => {
+        const { domElement } = renderer;
+        console.log(renderer);
+
+        domElement.id = 'threeCanvas'
+        domElement.className = 'main__canvas'
+        ref.current.appendChild(domElement)
+        // let canvas = document.createElement('canvas')
+        //TODO CREATE COMPONENT FOR CANVAS WITH document.createElement
     }
 
     render() {
+        //! If we wanna keep app size, but lower res, do this:
+        //-> this.renderer.setSize(window.innerWidth/2, window.innerHeight/2, false)
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
         return (
-            <main className='main container'>
-                <Blackhole/>
+            <main className='main container' ref={this?.threeContainer} onLoad={this.animate}>
             </main>
-            );
+        );
     }
 }
--- src/components/main/bodies/Blackhole.jsx
+++ src/components/main/bodies/Blackhole.jsx
@@ -1,19 +1,30 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import Projects from './ProjectList';
 import About from './About';
 
 export default class Blackhole extends Component {
-     constructor(props) {
-         super(props);
-     }
+    constructor(props) {
+        super(props);
+        this.initGeometry(this?.props?.Three)
+    }
+
+    initGeometry = (Three) => {
+        const geometry = new Three.BoxGeometry();
+        const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new Three.Mesh(geometry, material);
+        this.props.scene.add(cube)
+
+        this.props.camera.position.z = 10;
+    }
 
     render() {
-        return(
-            <div>
+        return (
+            <div className="">
                 <h1>Blackhole</h1>
-                <Projects/>
-                <About/>
+                <Projects />
+                <About />
             </div>
         )
+
     }
 }
