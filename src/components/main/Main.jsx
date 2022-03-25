1c1d25c67756cdb6a634a744ebb4492d859cdcb2
VCAngel <vargascasavantesangel.329@gmail.com>
2022-03-25 04:19:06 -0700

    Solar system structure pending

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,10 @@
 import React, { Component, useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import Blackhole from './bodies/Blackhole';
+import { OrbitControls } from '@react-three/drei';
+import { Blackhole, ProjectList, About } from './Bodies';
 
 //TODO threeJS models and scenes
+//TODO Creating groups and making the solar system structure
 export default class Main extends Component {
     constructor(props) {
         super(props);
@@ -11,10 +13,13 @@ export default class Main extends Component {
     render() {
         return (
             <main className='main container' ref={this?.threeContainer}>
-                <Canvas>
-                    <ambientLight/>
-                    <pointLight position={[10,10,10]}/>
-                    <Box />
+                <Canvas className='main__canvas'>
+                    <OrbitControls />
+                    <ambientLight intensity={0.5} />
+                    <pointLight position={[10, 10, 10]} />
+                    <Blackhole />
+                    <About />
+                    <ProjectList />
                 </Canvas>
             </main>
         )
@@ -26,18 +31,21 @@ const Box = (props) => {
     const mesh = useRef()
     // Set up state for the hovered and active state
     const [active, setActive] = useState(false)
+    const [hovered, setHovered] = useState(false)
     //-> Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x += 0.01))
-    useFrame(() => (mesh.current.rotation.y += 0.01))
+    // useFrame(() => (mesh.current.rotation.x += 0.01))
+    // useFrame(() => (mesh.current.rotation.y += 0.01))
 
     return (
         <mesh
             {...props}
             ref={mesh}
             scale={active ? 1.5 : 1}
-            onClick={(event) => setActive(!active)}>
+            onClick={(event) => setActive(!active)}
+            onPointerEnter={event => setHovered(!hovered)}
+            onPointerLeave={event => setHovered(!hovered)}>
             <boxBufferGeometry args={[2, 2, 2]} />
-            <meshLambertMaterial color={'hotpink'} />
+            <meshLambertMaterial color={hovered ? 'blue' : 'hotpink'} />
         </mesh>
     )
 }
--- src/components/main/bodies/Blackhole.jsx
+++ src/components/main/bodies/Blackhole.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
-import Projects from './ProjectList';
-import About from './About';
+import { Canvas } from '@react-three/fiber';
 
 export default class Blackhole extends Component {
     constructor(props) {
         super(props);
+        this.initGeometry();
+    }
+
+    initGeometry() {
+        //-> This ref will give us direct access to the mesh
+        this.mesh = React.createRef();
     }
 
     render() {
         return (
-            <div>
-                <h1>Blackhole</h1>
-                <Projects />
-                <About />
-            </div>
+            <mesh
+                ref={this.mesh}>
+                <sphereBufferGeometry args={[2]} />
+                <meshLambertMaterial color={'white'}/>
+            </mesh>
         )
 
     }
