8b6cd9d355ffda6d5c41bf1463eee65fefc484fc
VCAngel <vargascasavantesangel.329@gmail.com>
2022-03-29 13:26:53 -0700

    Footer/Contact styling done; gltf avatar pending

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,15 +3,37 @@ import Social from './Social';
 
 const Contact = () => {
     return (
-        <footer className="contact">
-            <h2 className="contact__title">Let's work together!</h2>
-            <h3 className="contact__email"><a href="#">vcangel00@gmail.com</a></h3>
-            <ul className="contact__socials">
-                <Social social='linkedIn' />
-                <Social social='github' />
-                <Social social='platzi' />
-                <Social social='twitter' />
-            </ul>
+        <footer className="container">
+            <section className="contact">
+                <div className="contact__title">
+                    <h2 className="contact__title--title">Let's work <span>together!</span></h2>
+                    <p className="contact__title--text">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Impedit atque nihil, cumque iure ipsa perferendis adipisci voluptates
+                        quisquam recusandae consequuntur eveniet rem. Cumque veritatis voluptatem
+                        voluptate. Vero soluta cumque maxime!
+                    </p>
+                </div>
+
+                <button className="btn contact__button">Get in touch</button>
+
+                <div className="contact__avatar">
+                    {/*TODO avatar model and import with Three package*/}
+                    <span>avatar</span>
+                </div>
+
+            </section>
+
+            <section className="end">
+                <ul className="end__socials">
+                    <Social social='linkedIn' />
+                    <Social social='github' />
+                    <Social social='platzi' />
+                    <Social social='twitter' />
+                </ul>
+                <p className="end__cr">©2022 Angel Vargas</p>
+                <p className="end__msg">the only limit is your imagination</p>
+            </section>
         </footer>
     )
 }
