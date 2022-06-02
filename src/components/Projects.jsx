5a83e3d071401428db5c522d28c8e4f1cd009d6d
VCAngel <vargascasavantesangel.329@gmail.com>
2022-06-02 13:45:09 -0600

    Styled projects component; project images pending, also css clean up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 
 export default function Projects(props) {
-    return(
+    const cards = [
+        { title: "CIMAV", text: "Heat Equation calculator. Uses Finite Element Method for displaying results through an assigned model.", href: "https://github.com/" },
+        { title: `House's Community`, text: "CRUD Application for resident management in a housing estate.", href: "https://github.com/VCAngel/House_Community" },
+        { title: "Github User Search", text: "Web application that consumes Github's API. Displays searched user's information", href: "https://github.com/VCAngel/GH_User_Search" }
+    ]
+
+    return (
         <section className="container" id="projects">
             <div className="projects">
-                <h2 className="projects--title">Check out <span>my works</span></h2>
+                <h2 className="projects--title">Here's some <span>stuff I made.</span></h2>
+                <p className="projects--mini">Made with care 🤍</p>
                 <div className="projects__showcase">
-
+                    {cards.map((item, index) => <ProjectCard title={item.title}
+                        text={item.text}
+                        href={item.href}
+                        key={index} />)}
                 </div>
             </div>
         </section>
+    )
+}
+
+function ProjectCard({ title, text, href }) {
+    return (
+        <div className="projects__showcase--card">
+            <div className="content">
+                <h3 className="title">{title}</h3>
+                <p className="text">{text}</p>
+                <a className="btn btn-glow" href={href} target="_blank">Source Code</a>
+            </div>
+        </div>
     )
 }
