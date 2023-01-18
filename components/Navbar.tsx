01da005ae53e99024bec735d4433b193476df137
Angel Vargas <vcangel00@gmail.com>
2023-01-18 15:53:34 -0600

    styling: Navbar/Footer correction

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,25 @@
-import { PageProps } from "$fresh/server.ts";
-
 const Navbar = () => {
 	const links = ['About', 'Projects', 'Contact']
 
 	return (
-		<header className="navbar">
-			<div className="navbar__icon">
-				<a href='/' onClick={() => console.log("home")}>VA</a>
+		<header>
+			<div className="navbar container">
+				<div className="navbar__icon">
+					<a href='/' onClick={() => console.log("home")}>VA</a>
+				</div>
+				<ul className="navbar__links">
+					<li className="navbar__links--item">
+						<a
+							target="_blank"
+							href="https://drive.google.com/file/d/1Vi25kKB6tjF77eY-m9L12IVCSseEafjH/view?usp=sharing">
+							Resume</a>
+					</li>
+
+					<span></span>
+
+					{links.map((link, index) => <Navlink text={link} key={index} />)}
+				</ul>
 			</div>
-			<ul className="navbar__links">
-				<li className="navbar__links--item">
-					<a
-						target="_blank"
-						href="https://drive.google.com/file/d/1Vi25kKB6tjF77eY-m9L12IVCSseEafjH/view?usp=sharing">
-						Resume</a>
-				</li>
-
-				<span></span>
-
-				{links.map((link, index) => <Navlink text={link} key={index} />)}
-			</ul>
 		</header>
 	)
 }
