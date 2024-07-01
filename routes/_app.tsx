8f879147974fad754a05f0e496cbc64617c523b4
VCAngel <vcangel00@gmail.com>
2024-07-01 16:54:15 -0600

    feat(seo): Add meta tags :sauropod:!

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,6 +9,22 @@ export default function App({ Component }: PageProps) {
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
+                <meta name="title" content="guest@vcangel" />
+                <meta
+                    name="description"
+                    content="Welcome to my portfolio! Feel free to explore around :)"
+                />
+                <meta
+                    name="keywords"
+                    content="portfolio,terminal,web,page,personal,resume,vcangel,github,socials,projects"
+                />
+                <meta name="robots" content="index, follow" />
+                <meta
+                    http-equiv="Content-Type"
+                    content="text/html; charset=utf-8"
+                />
+                <meta name="language" content="English" />
+                <meta name="author" content="vcangel" />
                 <link rel="preload" as="style" href="./css/normalize.css" />
                 <link rel="preload" as="style" href="./css/font-faces.css" />
                 <link rel="preload" as="style" href="./css/app.css" />
