c6e3efc41ebe556dda9da4363a93c1001eda3d64
VCAngel <vcangel00@gmail.com>
2024-07-18 11:35:18 -0600

    styles(console): Add basic panes structure
    To-do: Add 'About me' info & 'Current activity' badges

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -19,15 +19,26 @@ export default function Wrapper({ Component }: PageProps) {
           </Partial>
         </Terminal>
 
-        <section className="console-pane-wrapper lg:row-span-1 lg:row-start-1 lg:col-start-4 2xl:row-span-2">
-          <div className="console-pane">
+        <section className="console-pane-wrapper !flex-row lg:!flex-col gap-[2ch] p-[2ch] py-[1ch] lg:row-span-1 lg:row-start-1 lg:col-start-4 2xl:row-span-2">
+          <div class="flex-shrink-0">
+            <img
+              class="pixelated-image object-contain lg:flex-grow lg:w-full lg:h-auto lg:max-h-48 2xl:max-h-64"
+              src="https://avatars.githubusercontent.com/u/42756104?v=4"
+              alt="test"
+            />
+          </div>
+          <div class="border-l lg:border-b lg:border-l-0 border-slate-300 h-full lg:h-auto lg:w-full">
+          </div>
+          <div class="whitespace-pre-wrap">
+            {/* TODO Add info component */}
             About me!
           </div>
         </section>
 
         <aside className="console-pane-wrapper !hidden lg:!flex g:row-span-1 lg:row-start-2 lg:col-start-4 2xl:row-start-3">
-          <div className="console-pane">
-            current activity
+          <div class="console-pane">
+            {/* TODO Add Spotify component */}
+            Current activity
           </div>
         </aside>
       </div>
