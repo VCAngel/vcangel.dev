957ba1bb77b744c1cc34179244d09bd2bba54ce3
Ángel Vargas <vcangel00@gmail.com>
2026-06-10 15:24:24 -0600

    feat(commands): implement whoami with easter egg
    Replace placeholder with guest user line plus a playful identity easter egg
    
    Bump to 1.0.0-alpha.3.

--- src/commands/bin/whoami.tsx
+++ src/commands/bin/whoami.tsx
@@ -1,12 +1,29 @@
+import { TypewriterText } from "../../components/TypewriterText.tsx";
 import { CommandExecutor } from "../../models/command.model.ts";
 import { currentDirectory } from "../../state/app.state.ts";
+
+const EASTER_EGG =
+  "Idk, you tell me! m9っ`･ω･´)";
 
 export const whoAmICommand: CommandExecutor = (_args, fullCommand) => {
   return {
     command: fullCommand,
     route: currentDirectory.value,
     response: () => {
-      return <pre>Main command: WhoAmI</pre>;
+      return (
+        <ul className="command-wrapper">
+          <li>
+            <pre>
+              <TypewriterText text="guest" key="whoami_user" />
+            </pre>
+          </li>
+          <li>
+            <pre className="text-gray-400">
+              <TypewriterText text={EASTER_EGG} key="whoami_egg" speed={16} />
+            </pre>
+          </li>
+        </ul>
+      );
     },
   };
 };
