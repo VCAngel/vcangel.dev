0caad8bc8295fc3e8dfd10464307025db3391e2b
Ángel Vargas <vcangel00@gmail.com>
2026-06-11 16:02:38 -0600

    chore: remove build options
    debug build error and/or server error on deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,5 @@ import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [fresh(), tailwindcss()],
-  /* build: {
-    rolldownOptions: {
-      external: [
-        "fresh",
-        "fresh/internal",
-        "fresh/runtime",
-        "fresh/runtime-client",
-        "@fresh/plugin-vite/client",
-      ],
-    },
-  }, */
+  plugins: [fresh(), tailwindcss()]
 });
