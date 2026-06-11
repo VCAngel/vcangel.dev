a4078ec9e431fb59bae528cf40e7b3217954666d
Ángel Vargas <vcangel00@gmail.com>
2026-06-11 11:31:00 -0600

    refactor(fresh): remove trailling slashed on prod

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-import { App, staticFiles } from "fresh";
+import { App, staticFiles, trailingSlashes } from "fresh";
 
-export const app = new App()
+export const app = new App({ basePath: import.meta.url })
   // Add static file serving middleware
   .use(staticFiles())
   // Enable file-system based routing
-  .fsRoutes();
+  .fsRoutes()
+  // Handle trailling slashes in URLs
+  .use(trailingSlashes("never"));
