c653ecc7fbf896398610dd364b18b6e0a75956e8
Ángel Vargas <vcangel00@gmail.com>
2026-06-11 16:37:56 -0600

    fix(vite): build step error for previous deps
    clean dependencies for new `fresh@2.3.3` version bump

--- client.ts
+++ client.ts
@@ -1,4 +1,3 @@
 // Import CSS files here for hot module reloading to work.
-// normalize.css is imported by app.css into the base layer.
 import "./assets/css/font-faces.css";
 import "./assets/css/app.css";
